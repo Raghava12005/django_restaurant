eb447f3ee207c6dae713e70a1d97a599f6890256
Raghava12005 <147335470+Raghava12005@users.noreply.github.com>
2025-04-29 16:10:22 +0530

    Update wsgi.py

--- hello/wsgi.py
+++ hello/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
 
 application = get_wsgi_application()
+django.setup()
+call_command("migrate")
